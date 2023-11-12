94cfa43615489014eb6df9dbe3c41f543da23c4c
jeff <jebberwocky@gmail.com>
2023-11-12 16:50:52 +0800

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const p = 3301
+const port = process.env.PORT || p;
+const cors = require("cors");
+const config = require('config');
+const winston = require('winston'),expressWinston = require('express-winston');
+const bodyParser = require('body-parser');
+const corsoption = { origin: "*"}
+const { fetch } = require('node-fetch');
+var session = require('express-session')
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(corsoption));
+app.use(express.json());
+app.options('*', cors(corsoption))
+
+app.use(session({
+  secret: 'showmethemonkey',
+  resave: false,
+  saveUninitialized: true
+}))
+
+expressWinston.requestWhitelist.push('body');
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File(config.get('logs'))
+  ],
+}));
+
+app.use(expressWinston.errorLogger({
+  transports: [
+      new winston.transports.File(config.get('error-logs'))
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+  )
+}));
+
+const proxyRoutes = require("./routes/proxy")
+app.use('/', proxyRoutes);
+
+app.listen(port, () => {
+  console.log(`proxy node server is running on port ${port}`);
+});
---
+++ routes/proxy.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+    router = express.Router();
+const fetch = require('node-fetch')
+const {createWriteStream} = require("fs");
+
+router.get('/', (req, res) => {
+    var url = req.query.url,
+        mh = req.query.mh;
+    if(url){
+        console.log(url)
+        url = decodeURIComponent(url)
+        console.log(url)
+        try {
+            var imgfile = './img/img_'+mh+'_'+Date.now()+'.png'
+        fetch(url).then((actual) => {
+            actual.headers.forEach((v, n) => res.setHeader(n, v));
+            actual.body.pipe(createWriteStream(imgfile));
+            actual.body.pipe(res);
+        });}catch (error){
+            console.error(error);
+            res.error(error)
+        }
+    }else{
+        res.send('nothing')
+    }
+})
+
+module.exports = router;
